3a308c8a7b5668eeb5a756b9571ba9b32242ed9c
sudosuanjal <anjalehackchey101@gmail.com>
2026-02-08 17:16:04 +0530

    feat: added zod validation

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,5 @@
+import { weatherSchema } from "./schemas/weatherSchema";
+
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
@@ -5,5 +7,5 @@ export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
     `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely&appid=${API_KEY}`,
   );
   const data = await res.json();
-  return data;
+  return weatherSchema.parse(data);
 }
