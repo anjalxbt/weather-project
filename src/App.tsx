6056de4b4278b94984c172ff33d644a103080442
sudosuanjal <anjalehackchey101@gmail.com>
2026-02-10 22:56:08 +0530

    feat: card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { getWeather } from "./api";
+import Card from "./components/cards/Card";
+
 function App() {
-  return <div className="bg-amber-500 text-3xl">hello anjal</div>;
+  const { data } = useQuery({
+    queryKey: ["weather"],
+    queryFn: () => getWeather({ lat: 50, lon: 50 }),
+  });
+  return (
+    <>
+      <div className="flex flex-col gap-8">
+        <Card title="Current weather">
+          {JSON.stringify(data?.current).slice(0, 100)}
+        </Card>
+        <Card title="Hourly forecast">
+          {JSON.stringify(data?.hourly).slice(0, 100)}
+        </Card>
+        <Card title="Daily forecast">
+          {JSON.stringify(data?.daily).slice(0, 100)}
+        </Card>
+      </div>
+    </>
+  );
 }
 
 export default App;
